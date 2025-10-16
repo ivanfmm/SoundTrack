df629fe5fec4008acd8c1d480e077a39ce76910a
Ivan <ivan.f.mijares@gmail.com>
2025-10-15 20:41:32 -0700

    acabe los controladores

---
+++ SoundTrack.Server/Controllers/ReviewCommentsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoundTrack.Server.Services;
+
+namespace SoundTrack.Server.Controllers
+{
+    [ApiController]
+    [Route("[ReviewController]")]
+    public class ReviewCommentsController : Controller
+    {
+        private readonly ISoundTrackRepository _SoundTrackRepository;
+        public ReviewCommentsController(ISoundTrackRepository SoundTrackRepository)
+        {
+            _SoundTrackRepository = SoundTrackRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllReviewComments()
+        {
+            var reviewComments = await _SoundTrackRepository.GetAllReviewComments();
+            return Ok(reviewComments);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetReviewCommentById(int id)
+        {
+            var reviewComment = await _SoundTrackRepository.GetReviewCommentById(id);
+            if (reviewComment == null)
+            {
+                return NotFound();
+            }
+            return Ok(reviewComment);
+        }
+        [HttpPost]
+        public IActionResult AddReviewComment([FromBody] Models.ReviewComment reviewComment)
+        {
+            _SoundTrackRepository.addReviewComment(reviewComment);
+            return CreatedAtAction(nameof(GetReviewCommentById), new { id = reviewComment.Id }, reviewComment);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateReviewComment(int id, [FromBody] Models.ReviewComment reviewComment)
+        {
+            if (id != reviewComment.Id)
+            {
+                return BadRequest();
+            }
+            var existingReviewComment = _SoundTrackRepository.GetReviewCommentById(id).Result;
+            if (existingReviewComment == null)
+            {
+                return NotFound();
+            }
+            _SoundTrackRepository.updateReviewComment(reviewComment);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteReviewComment(int id)
+        {
+            var existingReviewComment = _SoundTrackRepository.GetReviewCommentById(id).Result;
+            if (existingReviewComment == null)
+            {
+                return NotFound();
+            }
+            _SoundTrackRepository.deleteReviewComment(existingReviewComment);
+            return NoContent();
+        }
+    }
+}
---
+++ SoundTrack.Server/Controllers/ReviewController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoundTrack.Server.Data;
+using SoundTrack.Server.Services;
+
+namespace SoundTrack.Server.Controllers
+{
+    [ApiController]
+    [Route("[ReviewController]")]
+    public class ReviewController : Controller
+    {
+        private readonly ISoundTrackRepository _SoundTrackRepository;
+        public ReviewController(ISoundTrackRepository SoundTrackRepository)
+        {
+            _SoundTrackRepository = SoundTrackRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllReviews()
+        {
+            var reviews = await _SoundTrackRepository.GetAllReviews();
+            return Ok(reviews);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetReviewById(int id)
+        {
+            var review = await _SoundTrackRepository.GetReviewById(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            return  Ok(review);
+        }
+
+        [HttpPost]
+        public IActionResult AddReview([FromBody] Models.Review review)
+        {
+            _SoundTrackRepository.addReview(review);
+            return CreatedAtAction(nameof(GetReviewById), new { id = review.Id }, review);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateReview(int id, [FromBody] Models.Review review)
+        {
+            if (id != review.Id)
+            {
+                return BadRequest();
+            }
+            var existingReview = _SoundTrackRepository.GetReviewById(id).Result;
+            if (existingReview == null)
+            {
+                return NotFound();
+            }
+            _SoundTrackRepository.updateReview(review);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteReview(int id)
+        {
+            var existingReview = _SoundTrackRepository.GetReviewById(id).Result;
+            if (existingReview == null)
+            {
+                return NotFound();
+            }
+            _SoundTrackRepository.deleteReview(existingReview);
+            return NoContent();
+        }
+    }
+}
